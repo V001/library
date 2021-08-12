c4090deb3a086eb701189964f446531950c4a542
v001 <Ruslan296>
2021-08-12 05:47:13 +0300

    initial

---
+++ scripts/Mocks/hall.sql
@@ -0,0 +1,5 @@
+insert into reader_halls (id, title) values (gen_random_uuid(),'hall_1');
+insert into reader_halls (id, title) values (gen_random_uuid(),'hall_2');
+insert into reader_halls (id, title) values (gen_random_uuid(),'hall_3');
+insert into reader_halls (id, title) values (gen_random_uuid(),'hall_4');
+insert into reader_halls (id, title) values (gen_random_uuid(),'hall_5');
